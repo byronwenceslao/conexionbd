b0ef9d2ecaa70eaa7ef3a3aa8682ae45076d632b
byronwenceslao <byronwenceslao97@users.noreply.github.com>
2019-11-03 21:37:09 -0600

    Esta es una prueba

---
+++ PruebaGithub/src/Prueba1/Github.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Byron González
+ */
+public class Github {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"HOLA");
+    }
+}
